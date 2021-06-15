a601583e5b087871627d2690b5477ee3f2b01676
Zach Coyle <zach.coyle@gmail.com>
2021-06-15 19:55:25 -0400

    add cabal2nix template

---
+++ cabal2nix/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    devshell.url = github:numtide/devshell;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ devshell.overlay ];
+      };
+    in
+    rec {
+
+      packages.hello = pkgs.hello;
+
+      defaultPackage = packages.hello;
+
+      devShell = pkgs.devshell.mkShell {
+        packages = with pkgs; [
+          cabal2nix
+          ghc
+          haskellPackages.cabal-install
+          stack
+        ];
+      };
+
+    });
+}
